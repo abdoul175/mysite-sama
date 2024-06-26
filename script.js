668812ddf815f03a36ac0ae636a7e8779b6f8420
abdoul175 <abdoulsaboursama51@gmail.com>
2024-06-26 08:36:58 +0000

    My Project 26-06-2024

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const menuBtn = document.querySelector('.menu')
 const items = document.querySelector('.navbar-items')
+const itemsElt = document.getElementsByClassName('navbar-item');
 
 menuBtn.onclick = (ev) => {
     items.classList.toggle('show')
+}
+
+for (const itemsEltElement of itemsElt) {
+    itemsEltElement.addEventListener('click', (ev) => {
+        if (ev.target.parentNode.parentNode.classList.contains('show')) {
+            ev.target.parentNode.parentNode.classList.toggle('show')
+        }
+        // console.log(ev.target.parentNode.parentNode);
+    })
 }
